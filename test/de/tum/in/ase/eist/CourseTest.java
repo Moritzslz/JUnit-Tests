9bbfffa60aaf3abe99a777c2ece896c551b87c5c
schultzm <schultzmoritz@gmail.com>
2023-07-03 10:55:47 +0200

    Tests

--- test/de/tum/in/ase/eist/CourseTest.java
+++ test/de/tum/in/ase/eist/CourseTest.java
@@ -7,7 +7,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class CourseTest {
 
 	// TODO 1: Test getCourseTitle()
+    @Test
+    public void testGetCourseTitle() {
+        Course course = new Course("EIST is the best");
+        assertEquals("EIST is the best", course.getTitle());
+    }
 
 	// TODO 2: Test getNumberOfAttendees()
+    @Test
+    public void testNoAttendees() {
+        Course course = new Course("EIST is the best");
+        assertEquals(0, course.getAttendees().size());
+    }
+
+    @Test
+    public void  testThreeAttendees() {
+        Course course = new Course("EIST is the best");
+        Student student1 = new Student("Max", "Mustermann", "01.01.2000", "CS", "Software Engineering");
+        Student student2 = new Student("Sophie", "Maier", "02.02.2002", "Mathe", "Lineare Algebra");
+        Student student3 = new Student("Ben", "Müller", "03.03.1999", "TUM BWL", "Micro Ökonomik");
+        course.addAttendee(student1);
+        course.addAttendee(student2);
+        course.addAttendee(student3);
+        assertEquals(3, course.getAttendees().size());
+    }
 
 }
--- test/de/tum/in/ase/eist/OnlineCourseTest.java
+++ test/de/tum/in/ase/eist/OnlineCourseTest.java
@@ -2,9 +2,25 @@ package de.tum.in.ase.eist;
 
 import org.junit.jupiter.api.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OnlineCourseTest {
 
 	// TODO 3: Test setOnlineCourseUrl()
+    @Test
+    public void  testSetOnlineCourseUrlWithValidUrl() throws MalformedURLException {
+        OnlineCourse onlineCourse = new OnlineCourse("EIST Moodle");
+        URL url = new URL("https://artemis.in.tum.de/courses/241/exercises/9427");
+        onlineCourse.setUrl("https://artemis.in.tum.de/courses/241/exercises/9427");
+        assertEquals(url, onlineCourse.getUrl());
+    }
+
+    @Test
+    public void  testSetOnlineCourseUrlWithInvalidUrl() throws MalformedURLException {
+        OnlineCourse onlineCourse = new OnlineCourse("EIST Moodle");
+        assertThrows(MalformedURLException.class, () -> onlineCourse.setUrl("invalidUrl"));
+    }
 }
